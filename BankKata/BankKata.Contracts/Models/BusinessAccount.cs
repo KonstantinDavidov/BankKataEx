87645495b17020c96c32fd6fbf34b59e9eac0c46
Konstantin Davydov <konstantin.davidov1@gmail.com>
2021-06-23 01:53:15 +0400

    Added BusinessAccount and GiroAccount types.

---
+++ BankKata/BankKata.Contracts/Models/BusinessAccount.cs
@@ -0,0 +1,19 @@
+﻿using BankKata.Contracts.Interfaces;
+
+namespace BankKata.Contracts.Models
+{
+	/// <summary>
+	/// Business 100,000.00
+	/// When creating a business account, a business id number must be provided
+	/// </summary>
+	public class BusinessAccount : Account
+	{
+		public int BusinessId { get; }
+
+		public BusinessAccount(int businessId, ITransactionStorage transactionStorage, IStatementPrinter statementPrinter)
+			: base(transactionStorage, statementPrinter)
+		{
+			BusinessId = businessId;
+		}
+	}
+}
---
+++ BankKata/BankKata.Contracts/Models/GiroAccount.cs
@@ -0,0 +1,15 @@
+﻿using BankKata.Contracts.Interfaces;
+
+namespace BankKata.Contracts.Models
+{
+	/// <summary>
+	/// Giro 4,000.00
+	/// </summary>
+	public class GiroAccount : Account
+	{
+		public GiroAccount(ITransactionStorage transactionStorage, IStatementPrinter statementPrinter)
+			: base(transactionStorage, statementPrinter)
+		{
+		}
+	}
+}
