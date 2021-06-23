06b64cd8d53d08b649bb86f8bb515dae9bf4e191
Konstantin Davydov <konstantin.davidov1@gmail.com>
2021-06-23 22:05:09 +0400

    Added unit tests for Clock.

---
+++ BankKata/BankKata.Tests/Fakes/ClockFake.cs
@@ -0,0 +1,10 @@
+﻿using BankKata.Contracts;
+using System;
+
+namespace BankKata.Tests.Fakes
+{
+	public class ClockFake : Clock
+	{
+		protected override DateTime DateNow => new DateTime(2099, 12, 07);
+	}
+}
---
+++ BankKata/BankKata.Tests/UnitTests/ClockShould.cs
@@ -0,0 +1,17 @@
+﻿using BankKata.Tests.Fakes;
+using NUnit.Framework;
+
+namespace BankKata.Tests.UnitTests
+{
+	public class ClockShould
+	{
+		[Test]
+		public void Clock_should_return_today_date_in_dd_MM_YYYY()
+		{
+			var clockMoq = new ClockFake();
+			var date = clockMoq.DateTimeNowAsString();
+
+			Assert.AreEqual("07/12/2099", date);
+		}
+	}
+}
